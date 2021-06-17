13d43fcfd21bf112c5b603ea07b70584f85b33e6
franciscoxaviersp <45402958+franciscoxaviersp@users.noreply.github.com>
2021-06-17 19:07:27 +0100

    Update featureExtraction.py

--- featureExtraction.py
+++ featureExtraction.py
@@ -2,8 +2,12 @@ import argparse
 import os
 
 import numpy as np
+import scalogram
 import matplotlib.pyplot as plt
 from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.decomposition import PCA
+from random import random
 
 
 def slidingObsWindow(data, lengthObsWindow, slidingValue):
@@ -74,38 +78,117 @@ def extractFeaturesSilence(data):
 
     return (np.array(features))
 
+def extractFeaturesWavelet(data,scales=[2,4,8,16,32],Class=0):
+    features=[]
+    nObs,nSamp,nCols=data.shape
+    for i in range(nObs):
+        scalo_features=np.array([])
+        for c in range(nCols):
+            scalo,fscales=scalogram.scalogramCWT(data[i,:,c],scales)
+            scalo_features=np.append(scalo_features,scalo)
+            
+        features.append(scalo_features)
+        
+    return np.array(features)
 
 def main():
     windows_all = np.array([])
     i = 0
+    nFiles = len(os.listdir('streams')) + len(os.listdir('streams05'))
+    testFiles = []
     for folder in ['streams', 'streams05']:
         for file in os.listdir(folder):
-            print(file)
-            fileInput = open('{}/{}'.format(folder, file), 'r')
-            data = np.loadtxt(fileInput, dtype=int)
-            windows = np.array(slidingObsWindow(data, 3, 1))
-            if i == 0:
-                windows_all = windows
+            
+            prob = random()
+            if prob < 0.7:
+                print(file)
+                fileInput = open('{}/{}'.format(folder, file), 'r')
+                data = np.loadtxt(fileInput, dtype=int)
+                windows = np.array(slidingObsWindow(data, 3, 1))
+                if i == 0:
+                    windows_all = windows
+                else:
+                    windows_all = np.concatenate((windows, windows_all))
+                i += 1
             else:
-                windows_all = np.concatenate((windows, windows_all))
-            i += 1
+                testFiles.append('{}/{}'.format(folder, file))
+
+    pca = PCA(n_components=3, svd_solver='full')
+
     features_timedependent = extractFeatures(windows_all)
     features_timeindependent = extractFeaturesSilence(windows_all)
-    features = np.hstack((features_timedependent, features_timeindependent))
+    #featuresW = extractFeaturesWavelet(windows_all)
+    train_features = np.hstack((features_timedependent, features_timeindependent))
+    trainScaler = MaxAbsScaler().fit(train_features)
+    train_featuresN=trainScaler.transform(train_features)
+    trainPCA=pca.fit(train_featuresN)
+    train_featuresNPCA = trainPCA.transform(train_featuresN)
+
     model = IsolationForest(n_estimators=50, max_samples='auto', contamination='auto', max_features=1.0)
-    model.fit(features)
+    model.fit(train_featuresNPCA)
+
+    totalAnom = 0
+    totalTests = 0
+    sizeAnom = 0
+    sizeOk = 0
+
+    for file in testFiles:
+        print(file)
+        test = np.loadtxt(open(file, 'r'), dtype=int)
+        windows = np.array(slidingObsWindow(test, 3, 1))
+        features_timedependent = extractFeatures(windows)
+        features_timeindependent = extractFeaturesSilence(windows)
+        #featuresW = extractFeaturesWavelet(windows)
+        test_features = np.hstack((features_timedependent, features_timeindependent))
+
+        test_featuresN=trainScaler.transform(test_features)
+        test_featuresNPCA = trainPCA.transform(test_featuresN)
+        #print(features)
+
+        a = model.predict(test_featuresNPCA)
+        b = model.decision_function(test_featuresNPCA)
+        #print(a)
+        #print(b)
+        totalTests += 1
+        anom = 0
+        if len(test)-3>30:
+            for prediction in a:
+                if prediction == -1:
+                    anom += 1
+            
+            if anom/len(a) >0.5:
+                totalAnom += 1
+                sizeAnom += len(test)-3
+                print(a)
+                print(b)
+                print("ANOMALY")
+            else:
+                sizeOk += len(test)-3
+                print("OK")
 
     test = np.loadtxt(open('streams2/3.txt', 'r'), dtype=int)
     windows = np.array(slidingObsWindow(test, 3, 1))
     features_timedependent = extractFeatures(windows)
     features_timeindependent = extractFeaturesSilence(windows)
-    features = np.hstack((features_timedependent, features_timeindependent))
-    print(features)
+    #featuresW = extractFeaturesWavelet(windows)
+    test_features = np.hstack((features_timedependent, features_timeindependent))
 
-    a = model.predict(features)
-    b = model.decision_function(features)
+    test_featuresN=trainScaler.transform(test_features)
+    test_featuresNPCA = trainPCA.transform(test_featuresN)
+    #print(features)
+
+    a = model.predict(test_featuresNPCA)
+    b = model.decision_function(test_featuresNPCA)
     print(a)
     print(b)
+
+    print("OK average stream time (seconds): "+ str(sizeOk/(totalTests-totalAnom)))
+    print("Anomalies average stream time (seconds): "+ str(sizeAnom/totalAnom))
+
+    print("Test size: "+str(totalTests))
+    print("Total anomalies: "+str(totalAnom))
+
+    print("Percentage of accurate prediction on test dataset: "+str(100-(totalAnom/totalTests)*100))
 
 
 if __name__ == '__main__':
