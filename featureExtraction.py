cc9d58af67593d6661b3222ec06639fecd3305d2
franciscoxaviersp <45402958+franciscoxaviersp@users.noreply.github.com>
2021-06-23 16:41:41 +0100

    Update featureExtraction.py

--- featureExtraction.py
+++ featureExtraction.py
@@ -1,13 +1,13 @@
-import argparse
-import os
 
-import numpy as np
-import scalogram
+import os
+import pickle
+from random import random
+
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.decomposition import PCA
 from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import MaxAbsScaler
-from sklearn.decomposition import PCA
-from random import random
 
 
 def slidingObsWindow(data, lengthObsWindow, slidingValue):
@@ -78,18 +78,6 @@ def extractFeaturesSilence(data):
 
     return (np.array(features))
 
-def extractFeaturesWavelet(data,scales=[2,4,8,16,32],Class=0):
-    features=[]
-    nObs,nSamp,nCols=data.shape
-    for i in range(nObs):
-        scalo_features=np.array([])
-        for c in range(nCols):
-            scalo,fscales=scalogram.scalogramCWT(data[i,:,c],scales)
-            scalo_features=np.append(scalo_features,scalo)
-            
-        features.append(scalo_features)
-        
-    return np.array(features)
 
 def main():
     windows_all = np.array([])
@@ -117,7 +105,6 @@ def main():
 
     features_timedependent = extractFeatures(windows_all)
     features_timeindependent = extractFeaturesSilence(windows_all)
-    #featuresW = extractFeaturesWavelet(windows_all)
     train_features = np.hstack((features_timedependent, features_timeindependent))
     trainScaler = MaxAbsScaler().fit(train_features)
     train_featuresN=trainScaler.transform(train_features)
@@ -127,83 +114,53 @@ def main():
     model = IsolationForest(n_estimators=50, max_samples='auto', contamination='auto', max_features=1.0)
     model.fit(train_featuresNPCA)
 
+    with open("scaler","wb") as f:
+        pickle.dump(trainScaler,f)
+        f.close()
+    with open("pca","wb") as f:
+        pickle.dump(trainPCA,f)
+        f.close()
+    with open("model","wb") as f:
+        pickle.dump(model,f)
+        f.close()
+
     totalAnom = 0
     totalTests = 0
     sizeAnom = 0
     sizeOk = 0
 
-    testFiles.append("streams2/1.txt")
-    testFiles.append("streams2/3.txt")
-    all_scores = np.array([])
+    with open('model', 'rb') as pickle_file:
+        model = pickle.load(pickle_file)
+    
     for file in testFiles:
         print(file)
         test = np.loadtxt(open(file, 'r'), dtype=int)
         windows = np.array(slidingObsWindow(test, 3, 1))
         features_timedependent = extractFeatures(windows)
         features_timeindependent = extractFeaturesSilence(windows)
-        #featuresW = extractFeaturesWavelet(windows)
         test_features = np.hstack((features_timedependent, features_timeindependent))
 
         test_featuresN=trainScaler.transform(test_features)
         test_featuresNPCA = trainPCA.transform(test_featuresN)
-        #print(features)
 
         a = model.predict(test_featuresNPCA)
         b = model.decision_function(test_featuresNPCA)
-        all_scores = np.concatenate((b, all_scores))
-        #print(a)
-        #print(b)
+
         totalTests += 1
         anom = 0
-        if len(test)-3>15:
+        if len(test)-3>0: #change 0 to minimum session time
             for i,prediction in enumerate(a):
-                if prediction == -1 and b[i] <= -0.125:
+                if prediction == -1 and b[i] <= 0:  #increase 0 to be more strict(more anomalies), decrease to be less strict(less anomalies)
                     anom += 1
             
             if anom/len(a) >0.5:
                 totalAnom += 1
                 sizeAnom += len(test)-3
-                print(a)
-                print(b)
                 print("ANOMALY")
             else:
                 sizeOk += len(test)-3
                 print("OK")
 
-    all_scores = all_scores[all_scores < 0]
-    test = np.loadtxt(open('streams2/3.txt', 'r'), dtype=int)
-    windows = np.array(slidingObsWindow(test, 3, 1))
-    features_timedependent = extractFeatures(windows)
-    features_timeindependent = extractFeaturesSilence(windows)
-    #featuresW = extractFeaturesWavelet(windows)
-    test_features = np.hstack((features_timedependent, features_timeindependent))
-
-    test_featuresN=trainScaler.transform(test_features)
-    test_featuresNPCA = trainPCA.transform(test_featuresN)
-    #print(features)
-
-    a = model.predict(test_featuresNPCA)
-    b = model.decision_function(test_featuresNPCA)
-
-    print(a)
-    print(b)
-
-    test = np.loadtxt(open('streams2/1.txt', 'r'), dtype=int)
-    windows = np.array(slidingObsWindow(test, 3, 1))
-    features_timedependent = extractFeatures(windows)
-    features_timeindependent = extractFeaturesSilence(windows)
-    # featuresW = extractFeaturesWavelet(windows)
-    test_features = np.hstack((features_timedependent, features_timeindependent))
-
-    test_featuresN = trainScaler.transform(test_features)
-    test_featuresNPCA = trainPCA.transform(test_featuresN)
-    # print(features)
-
-    a = model.predict(test_featuresNPCA)
-    b = model.decision_function(test_featuresNPCA)
-    print(np.average(b))
-    print(a)
-    print(b)
 
     print("OK average stream time (seconds): "+ str(sizeOk/(totalTests-totalAnom)))
     print("Anomalies average stream time (seconds): "+ str(sizeAnom/totalAnom))
